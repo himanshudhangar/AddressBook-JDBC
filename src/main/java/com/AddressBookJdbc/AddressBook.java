e3b247115e299da71537460871c8ffa82abc9370
Git with himanshu <dhangarhimanshu450@gmail.com>
2022-05-10 14:38:36 +0530

    Added:Retrieve All Entries From DataBase

---
+++ src/main/java/com/AddressBookJdbc/AddressBook.java
@@ -0,0 +1,32 @@
+package com.AddressBookJdbc;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class AddressBook {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        boolean exit = false;
+        while (!exit) {
+            System.out.println(" Press\n 1 ->  Retrieve data\n 2 -> Exit");
+            switch (scanner.nextInt()) {
+                case 1:
+                    retrieveData();
+                    break;
+                case 2:
+                    exit = true;
+            }
+        }
+    }
+
+    private static void retrieveData() {
+        AddressBookRepo addressBookRepo = new AddressBookRepo();
+        List<Contacts> employeeInfoList = addressBookRepo.retrieveData();
+        for (Contacts employee : employeeInfoList
+        ) {
+            System.out.println(employee + "\n");
+        }
+    }
+}
---
+++ src/main/java/com/AddressBookJdbc/AddressBookSystem.java
@@ -0,0 +1,57 @@
+package com.AddressBookJdbc;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBookSystem {
+    Connection connection;
+
+    private Connection getConnection() {
+        String URL_JD = "jdbc:mysql://localhost:3306/payroll_service";
+        String USER_NAME = "root";
+        String PASSWORD = "Deepak@7";
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Drivers loaded!!");
+            connection = DriverManager.getConnection(URL_JD,USER_NAME,PASSWORD);
+            System.out.println("connection Established!!");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+
+    public List<Contacts> retrieveData() {
+        ResultSet resultSet = null;
+        List<Contacts> addressBookList = new ArrayList<Contacts>();
+        try (Connection connection = getConnection()) {
+            Statement statement = connection.createStatement();
+            String sql = "select * from AddressBook";
+            resultSet = statement.executeQuery(sql);
+            int count = 0;
+            while (resultSet.next()) {
+                Contacts contactInfo = new Contacts();
+                contactInfo.setFirstName(resultSet.getString("firstName"));
+                contactInfo.setLastName(resultSet.getString("Lastname"));
+                contactInfo.setAddress(resultSet.getString("address"));
+                contactInfo.setCity(resultSet.getString("city"));
+                contactInfo.setState(resultSet.getString("state"));
+                contactInfo.setZip(resultSet.getInt("zip"));
+                contactInfo.setPhoneNumber(resultSet.getString("phoneNumber"));
+                contactInfo.setEmailId(resultSet.getString("email"));
+                contactInfo.setBookName(resultSet.getString("bookName"));
+                contactInfo.setContactType(resultSet.getString("contactType"));
+                contactInfo.setDateAdded(resultSet.getDate("Date_added").toLocalDate());
+
+                addressBookList.add(contactInfo);
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return addressBookList;
+
+    }
+}
